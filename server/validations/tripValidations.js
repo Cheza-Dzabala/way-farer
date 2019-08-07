791e5211dfc8c21b121781dd14ba1fc934e0fcfd
Cheza Dzabala <dzabalamacheza@gmail.com>
2019-08-07 06:21:29 +0200

    Chore Fix Validation issues in trips
    - Added validation for double booking a bus
    
    Added validation for empty strings
    
    - Added validation for ensuring positive integers
    
    [delivers #167744150, #167594397]

--- server/validations/tripValidations.js
+++ server/validations/tripValidations.js
@@ -4,8 +4,9 @@ const validateNewTrip = (data) => {
   const schema = Joi.object().keys({
     origin: Joi.string().trim().regex(/^[a-zA-Z]{3,15}$/).required(),
     destination: Joi.string().trim().regex(/^[a-zA-Z]{3,15}$/).required(),
-    fare: Joi.number().min(1000).max(20000).required(),
-    seating_capacity: Joi.number().min(10).max(50).required(),
+    fare: Joi.number().positive().max(500000).required(),
+    seating_capacity: Joi.number().integer().positive().max(100)
+      .required(),
     trip_date: Joi.date().min('now').required(),
     bus_license_number: Joi.string().required(),
   });
