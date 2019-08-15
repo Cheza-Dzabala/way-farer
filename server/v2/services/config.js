f10b5947e1efadcad659b1e3723ffb1525db13ed
Cheza Dzabala <dzabalamacheza@gmail.com>
2019-08-15 15:19:22 +0200

    Heroku ENV FIX

--- server/v2/services/config.js
+++ server/v2/services/config.js
@@ -2,13 +2,37 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
+
 const { NODE_ENV } = process.env;
+let user;
+let database;
+let password;
+let port;
+let url;
+let max;
+let idleTimeoutMillis;
+
+if (NODE_ENV === 'production') {
+  user = process.env.HEROKU_USER;
+  database = process.env.HEROKU_DB;
+  password = process.env.HEROKU_PASSWORD;
+  port = process.env.HEROKU_PORT;
+  url = process.env.HEROKU_URI;
+} else if (NODE_ENV === 'test' || NODE_ENV === 'dev') {
+  user = process.env.PGUSER; // this is the db user credential
+  database = (NODE_ENV === 'test' ? process.env.PG_TEST_DATABASE : process.env.PGDB);
+  password = process.env.PGPASSWORD;
+  port = process.env.PGPORT;
+  max = process.env.PGMAX; // max number of clients in the pool
+  idleTimeoutMillis = process.env.PGTIMEOUT;
+}
+
 const config = {
-  user: process.env.PGUSER, // this is the db user credential
-  database: (NODE_ENV === 'test' ? process.env.PG_TEST_DATABASE : process.env.PGDB),
-  password: process.env.PGPASSWORD,
-  port: process.env.PGPORT,
-  max: process.env.PGMAX, // max number of clients in the pool
-  idleTimeoutMillis: process.env.PGTIMEOUT,
+  user,
+  password,
+  database,
+  port,
+  url,
 };
+
 module.exports = config;
