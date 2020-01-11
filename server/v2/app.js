6364d2069d1b60c8d51b3b0893f0c8abf471c1da
Macheza Dzabala <dzabalamacheza@gmail.com>
2020-01-11 19:45:46 +0200

    FT: CORS FIX

--- server/v2/app.js
+++ server/v2/app.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import morgan from 'morgan';
 import dotenv from 'dotenv';
+import cors from 'cors';
 import router from './routes/index';
 
 const version = 'v2';
@@ -11,7 +12,7 @@ dotenv.config();
 app.use(morgan('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-
+app.use(cors());
 
 app.use(`/api/${version}/`, router);
 
